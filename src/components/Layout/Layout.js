93d37ca2ab3d09122c0325020ef25d242fc35be8
Matheus Ferraresi <matheusferraresi@live.com>
2018-09-02 22:47:54 +0100

    Changing Auxiliary component name to fix windows incompatibility problem

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Aux from "../../hoc/Aux";
+import Auxi from "../../hoc/Auxi";
 import classes from "./Layout.css";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
 import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
@@ -21,11 +21,11 @@ export default class Layout extends Component {
 
   render() {
     return (
-      <Aux>
+      <Auxi>
         <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler}/>
         <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler} />
         <main className={classes.Content}>{this.props.children}</main>
-      </Aux>
+      </Auxi>
     );
   }
 }
